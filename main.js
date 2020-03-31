9e687817854f65af8420dd0bb2400754acb6f66b
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-03-31 06:37:25 -0400

    25 passed, 25 total; early in the morning is nice way to code on the fresh start

--- main.js
+++ main.js
@@ -1,15 +1,94 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const firstNumberIsLess = function(num1, num2) {
+  if (num1 < num2) {
+    return -1;
+  } else if (num1 > num2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
+const secondNumberIsLess = function(num1, num2) {
+  if (num2 < num1) {
+    return -1;
+  } else if (num2 > num1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
+const firstNameIsFirstAlphabetically = function(name1, name2) {
+  if (name1 < name2) {
+    return -1;
+  } else if (name1 > name2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+const firstLastNameIsFirstAlphabetically = function(name1, name2) {
+
+  let lastName1 = name1.slice(name1.indexOf(' ') + 1);
+  let lastName2 = name2.slice(name2.indexOf(' ') + 1);
+
+  if (lastName1 < lastName2) {
+    return -1;
+  } else if (lastName1 > lastName2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+const trueIsFirst = function(bool1, bool2) {
+  if (bool1 === true && bool2 === false) {
+    return -1;
+  } else if (bool1 === false && bool2 === true) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
 
 /************************
-// ITERATION FUNCTIONS
+ // ITERATION FUNCTIONS
  ************************/
 
+const sortByNumberAscending = function(nums) {
 
+  const sortedNumsAscending = nums.slice();
+  return sortedNumsAscending.sort(firstNumberIsLess);
+}
+
+const sortByNumberDescending = function(nums) {
+
+  const sortedNumsDescending = nums.slice();
+  return sortedNumsDescending.sort(secondNumberIsLess);
+}
+
+const truesFirst = function(bools) {
+  
+  const sortedBoolsFirstTrue = bools.slice();
+  return sortedBoolsFirstTrue.sort(trueIsFirst);
+}
+
+const sortByNameAscending = function(names) {
+
+  const sortedFirstNamesAscending = names.slice();
+  return sortedFirstNamesAscending.sort(firstNameIsFirstAlphabetically);
+}
+
+const sortByLastNameAscending = function(names) {
+
+  const sortedLastNamesAscending = names.slice();
+  return sortedLastNamesAscending.sort(firstLastNameIsFirstAlphabetically);
+}
 
 
 /*********************************
